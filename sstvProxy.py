259042fc69a29cdd52bb6daa82e2190d86dca90d
vorghahn <vaughan.crap@gmail.com>
2018-01-11 12:31:26 +1300

    attempt 2

--- sstvProxy.py
+++ sstvProxy.py
@@ -2614,7 +2614,10 @@ def auto(request_file, qual=QUAL):
 	logger.info("Channel %s playlist was requested by %s", channel,
 			request.environ.get('REMOTE_ADDR'))
 	sanitized_channel = ("0%d" % int(channel)) if int(channel) < 10 else channel
-	sanitized_qual = '1' if int(channel) > 60 else qual
+
+	sanitized_qual = '1'
+	if int(channel) <= 60:
+		sanitized_qual = qual
 	template = "https://{0}.smoothstreams.tv:443/{1}/ch{2}q{3}.stream/playlist.m3u8?wmsAuthSign={4}"
 	url = template.format(SRVR, SITE, sanitized_channel,sanitized_qual, token['hash'])
 	logger.debug("sanitized_channel: %s sanitized_qual: %s QUAL: %s qual: %s" % (sanitized_channel,sanitized_qual, QUAL, qual))
